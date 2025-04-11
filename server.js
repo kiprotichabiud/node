6b75ba7bff90b0a67377f8142deb13f45174fbdf
Kiprotich Abiud <kiprotichabiud1@gmail.com>
2025-04-11 20:01:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,4 @@ const server = http.createServer((req, res) => {
     res.end('Hello, Node.js Server!');
 });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
-});
+
